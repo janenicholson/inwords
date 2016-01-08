2b02fc5df4b9c22859facd0efbb33e924332e7ef
Jane Nicholson <jnicholson@iname.com>
2016-01-08 18:02:13 +0000

    Prevent crash

--- src/main/java/inwords/NumberFormatter.java
+++ src/main/java/inwords/NumberFormatter.java
@@ -3,6 +3,8 @@ package inwords;
 public class NumberFormatter {
 	private final String[] namesOfNumbers = { "zero", "one" };
 	public String inWords(Integer number) {
-		return (namesOfNumbers[number]);
+		if (number < namesOfNumbers.length)
+			return (namesOfNumbers[number]);
+		return "NYI";
 	}
 }
