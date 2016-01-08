52da5b3217eaa91698a883fd6f77bf7ca65c82d8
Jane Nicholson <jnicholson@iname.com>
2016-01-08 20:15:08 +0000

    Refactor tests to NumberCombiner

---
+++ src/main/java/inwords/NumberCombinerTest.java
@@ -0,0 +1,34 @@
+package inwords;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberCombinerTest {
+
+	private NumberFragment zeroFragment = new TensNumberFragment(0);
+	private NumberFragment mockFragment = new MockNumberFragment();
+
+	@Test
+	public void provide_ones_digit_when_only_ones_value_is_present() {
+		assertThat(new NumberCombiner().combine(zeroFragment, mockFragment), is("mock"));
+	}
+
+	@Test
+	public void provide_tens_and_ones_digit_when_only_tens_and_ones_value_is_present() {
+		assertThat(new NumberCombiner().combine(mockFragment, mockFragment), is("mock mock"));
+	}
+
+	private static class MockNumberFragment implements NumberFragment {
+
+		public String inWords() {
+			return "mock";
+		}
+
+		public boolean isPlaceHolder() {
+			return false;
+		}
+		
+	}
+}
