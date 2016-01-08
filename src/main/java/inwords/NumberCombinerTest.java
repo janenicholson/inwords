f20a29085740b3f1d3d4ed523d091519e066255c
Jane Nicholson <jnicholson@iname.com>
2016-01-08 20:18:10 +0000

    New failing test for tens digit in isolation

--- src/main/java/inwords/NumberCombinerTest.java
+++ src/main/java/inwords/NumberCombinerTest.java
@@ -12,12 +12,17 @@ public class NumberCombinerTest {
 
 	@Test
 	public void provide_ones_digit_when_only_ones_value_is_present() {
-		assertThat(new NumberCombiner().combine(zeroFragment, mockFragment), is("mock"));
+		assertThat(new NumberCombiner().combine(zeroFragment, zeroFragment, mockFragment), is("mock"));
 	}
 
 	@Test
 	public void provide_tens_and_ones_digit_when_only_tens_and_ones_value_is_present() {
-		assertThat(new NumberCombiner().combine(mockFragment, mockFragment), is("mock mock"));
+		assertThat(new NumberCombiner().combine(zeroFragment, mockFragment, mockFragment), is("mock mock"));
+	}
+
+	@Test
+	public void provide_tens_digit_when_only_tens_digit_is_present() {
+		assertThat(new NumberCombiner().combine(zeroFragment, mockFragment, zeroFragment), is("mock"));
 	}
 
 	private static class MockNumberFragment implements NumberFragment {
