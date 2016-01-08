3da5603d8df4188c29f8cb5cfc8b986b923cfca1
Jane Nicholson <jnicholson@iname.com>
2016-01-08 18:16:50 +0000

    Refactor our NumberFormatter knowing about number names directly

--- src/main/java/inwords/NumberCombiner.java
+++ src/main/java/inwords/NumberCombiner.java
@@ -3,7 +3,9 @@ package inwords;
 public class NumberCombiner {
 
 	public String combine(NumberFragment largeDigit, NumberFragment smallDigit) {
-		return String.format("%s %s", largeDigit.inWords(), smallDigit.inWords());
+		String largeDigitInWords = largeDigit.inWords();
+		String separator = largeDigitInWords.equals("") ? "" : " ";
+		return String.format("%s%s%s", largeDigit.inWords(), separator, smallDigit.inWords());
 	}
 
 }
--- src/main/java/inwords/NumberFormatter.java
+++ src/main/java/inwords/NumberFormatter.java
@@ -1,10 +1,7 @@
 package inwords;
 
 public class NumberFormatter {
-	private final String[] namesOfNumbers = { "zero", "one" };
 	public String inWords(Integer number) {
-		if (number < namesOfNumbers.length)
-			return (namesOfNumbers[number]);
 		NumberFragment smallDigit = new OnesNumberFragment(number%10);
 		NumberFragment largeDigit = new TensNumberFragment(number/10);
 		return new NumberCombiner().combine(largeDigit, smallDigit);
