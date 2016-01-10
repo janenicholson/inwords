7043c07109ffaa8d8208ed52509d0f0efbfd605e
Jane Nicholson <jnicholson@iname.com>
2016-01-10 14:00:19 +0000

    failing test for millions

--- src/test/java/inwords/NumberFormatterTest.java
+++ src/test/java/inwords/NumberFormatterTest.java
@@ -28,7 +28,8 @@ public class NumberFormatterTest {
 			{1042, "one thousand and forty two"},
 			{1105, "one thousand one hundred and five"},
 			{45781, "forty five thousand seven hundred and eighty one"},
-			{945781, "nine hundred and forty five thousand seven hundred and eighty one"}
+			{945781, "nine hundred and forty five thousand seven hundred and eighty one"},
+			{56945781, "fifty six million nine hundred and forty five thousand seven hundred and eighty one"}
 		});
 	}
 
