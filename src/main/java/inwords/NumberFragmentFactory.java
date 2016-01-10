c067660596b3e3b3aab68925abf83e0c66fcb952
Jane Nicholson <jnicholson@iname.com>
2016-01-10 12:02:13 +0000

    Make test pass

--- src/main/java/inwords/NumberFragmentFactory.java
+++ src/main/java/inwords/NumberFragmentFactory.java
@@ -1,9 +1,10 @@
 package inwords;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class NumberFragmentFactory {
 	public static List<NumberFragment> create(int number) {
-		return null;
+		return Arrays.<NumberFragment>asList(new OnesNumberFragment(number));
 	}
 }
