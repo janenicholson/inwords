90cabd0a8dcbb6d6138cc2e2d2ea132d654c3ce5
Jane Nicholson <jnicholson@iname.com>
2016-01-10 12:21:13 +0000

    Refactor so special case for 0 is inside stream operation

--- src/main/java/inwords/NumberFragmentFactory.java
+++ src/main/java/inwords/NumberFragmentFactory.java
@@ -2,15 +2,23 @@ package inwords;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class NumberFragmentFactory {
 	public static List<NumberFragment> create(int number) {
-		if (number == 0)
-			return Arrays.asList(new OnesNumberFragment(0));
 		return Arrays.stream(createFromNumber(number))
 				.filter(li -> !li.isPlaceHolder())
-				.collect(Collectors.toList());
+				.collect(Collectors.collectingAndThen(Collectors.toList(), placeHolderForZero()));
+	}
+
+	private static Function<List<NumberFragment>, List<NumberFragment>> placeHolderForZero() {
+		return new Function<List<NumberFragment>, List<NumberFragment>>() {
+			@Override
+			public List<NumberFragment> apply(List<NumberFragment> t) {
+				return (t.size() != 0) ? t : Arrays.<NumberFragment>asList(new OnesNumberFragment(0));
+			}
+		};
 	}
 
 	private static NumberFragment[] createFromNumber(int number) {
